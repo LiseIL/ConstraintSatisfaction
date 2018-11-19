c31bd309d0380442464a6e81db6e86ba61efce3a
Auhalf <44766869+Auhalf@users.noreply.github.com>
2018-11-19 18:37:13 +0100

    Update AC.py

--- AC.py
+++ AC.py
@@ -3,21 +3,22 @@ __Filename__ = 'AC.py'
 __Creationdate__ = '04/11/18'
 
 #######################ALGORITHME AC#####################
-from genererJeuDeDonnees import*
 from copy import *
 import numpy as np
 
-def WithoutSupport_AC(i,j,b, jeuDD):
+
+def WithoutSupport_AC(i, j, b, jeuDD):
     #i et j sont des entiers, b est une valeur. Renvoie un booleen
     #Si b dans Dj possède un support dans Di, la fonction renvoie False
     #Sinon elle renvoie True
     domaine_i = jeuDD[1][i]
     if len(domaine_i) == 0:
-        return 'EmptyDomain'
+        return 'EmptyDomain', i
     a = domaine_i[0]
-    while a < domaine_i[-1] and jeuDD[i, j, a, b] == 0:
+    while (a < domaine_i[-1]) and ((jeuDD[0])[i, j, a, b] == 0):
         a = domaine_i[domaine_i.index(a) + 1]
-    return not(jeuDD[i, j, a, b]==1)
+    return not(jeuDD[0][i, j, a, b] == 1)
+
 
 def initialisation_AC(jeuDD):
     if isinstance(jeuDD, np.ndarray):
@@ -27,35 +28,37 @@ def initialisation_AC(jeuDD):
             liste2 += [i]
         for i in range(jeuDD.shape[0]):
             liste += [copy(liste2)]
-        jeuDD = (jeuDD, liste)
+        jeuDD = tuple((jeuDD, liste))
     n = jeuDD[0].shape[0]
     List_AC = []
     Status_AC = [False]*n   
     for i in range(n): 
-        for j in range(n): 
+        for j in range(n):
             relation_ij = jeuDD[0][i, j]
             if not(np.array_equal(jeuDD[0][i, j], np.ones(relation_ij.shape))):
                 Dj = jeuDD[1][j]
                 if len(Dj) == 0:
-                    return 'EmptyDomain'   
+                    return 'EmptyDomain', j
                 for b in Dj:
                     if WithoutSupport_AC(i, j, b, jeuDD):
-                        if WithoutSupport_AC(i, j, b, jeuDD) == 'EmptyDomain':
-                            return 'EmptyDomain', i
+                        if isinstance(WithoutSupport_AC(i, j, b, jeuDD), tuple):
+                            if WithoutSupport_AC(i, j, b, jeuDD)[0] == 'EmptyDomain':
+                                return 'EmptyDomain', i
                         Dj.remove(b)
                         if not(Status_AC[j]):
                             List_AC += [j]
                             Status_AC[j] = True
-    return(List_AC, Status_AC)
+    return jeuDD, List_AC, Status_AC
+
 
 def propager_AC(i, List_AC, Status_AC, jeuDD):
     n = jeuDD[0].shape[0]
     for j in range(n): 
             relation_ij = jeuDD[0][i, j]
-            if not(np.array_equal(jeuDD[i, j], np.ones(relation_ij.shape))):
+            if not(np.array_equal(jeuDD[0][i, j], np.ones(relation_ij.shape))):
                 Dj = jeuDD[1][j]
                 if len(Dj) == 0:
-                   return 'EmptyDomain'
+                   return 'EmptyDomain', j
                 for b in Dj: 
                     if WithoutSupport_AC(i, j, b, jeuDD):
                         Dj.remove(b)
@@ -63,17 +66,20 @@ def propager_AC(i, List_AC, Status_AC, jeuDD):
                             List_AC += [j]
                             Status_AC[j] = True
 
+
 def algo_AC8(jeuDD):
     init = initialisation_AC(jeuDD)
-    if isinstance(init, str):
+    if isinstance(init[0], str):
         return init
-    elif isinstance(init, tuple)
-        List_AC = init[0]
-        Status_AC = init[1]          
-        while List_AC != []:
+    elif isinstance(init, tuple):
+        jeuDD = init[0]
+        List_AC = init[1]
+        Status_AC = init[2]
+        while len(List_AC) > 0:
             i = List_AC[0]
-            List_AC = List_AC.remove(i)
+            List_AC.remove(i)
             Status_AC[i] = False
             a = propager_AC(i, List_AC, Status_AC, jeuDD)
-            if a == 'EmptyDomain':
+            if isinstance(a, tuple) and a[0] == 'EmptyDomain':
                 return a
+    return jeuDD
