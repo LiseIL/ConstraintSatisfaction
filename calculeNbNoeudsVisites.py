deca0d7304f9569d6a85a54df2413dcfbe5c0a33
LiseIL <43322583+LiseIL@users.noreply.github.com>
2018-12-01 15:14:46 +0100

    Update calculeNbNoeudsVisites.py

--- calculeNbNoeudsVisites.py
+++ calculeNbNoeudsVisites.py
@@ -31,3 +31,47 @@ def compatibleAllBefore(Xi,i,JeuDeDonnee,solution):
         if JeuDeDonnee[Xk,Xi][solution[Xk]][i]==0:
             return False
     return True
+
+##################BT+AC ~ version renvoyant le nombre de noeuds parcourus##########################
+from BTAC import *
+
+def compteNoeudBTAC(solution, Xappel, JeuDeDonnee, noeudVisites):
+    #Input
+    #-----
+    #solution: une liste de n zéros, où n est le nombre de variables dans le JeuDeDonnee
+    #Xappel : la première variable appelée lors de de l'application du backtracking
+    #JeuDeDonnee : le jeu de données sous forme de np.array
+    #noeudVisites : une liste vide
+    
+    #Output
+    #------
+    #renvoie un tuple : (bool, int), le booleen indique True s'il existe une solution, et False sinon
+    #                                l'entier indique le nombre de noeux visités par l'algo de recherche
+    if isinstance(JeuDeDonnee, np.ndarray):
+        liste = []
+        liste2 = []
+        for i in range(JeuDeDonnee.shape[2]):
+            liste2 += [i]
+        for i in range(JeuDeDonnee.shape[0]):
+            liste += [copy(liste2)]
+        JeuDeDonnee = tuple((JeuDeDonnee, liste))
+    valeurs = []
+    for i in range(Xappel):
+        valeurs += [[solution[i]]]
+    valeurs += JeuDeDonnee[1][Xappel:]
+    JDD = algo_AC8((JeuDeDonnee[0], valeurs))
+    if isinstance(JDD[0], str):
+        return (False, len(noeudVisites))
+    if Xappel >= len(valeurs):
+        # cas terminal
+        print(solution)
+        return (True, len(noeudVisites))
+    for i in valeurs[Xappel]:
+        noeudVisites += [(Xappel, i)]
+        valeurs2 = deepcopy(valeurs)
+        if compatibleAllBefore(Xappel, i, JDD, solution):
+            solution[Xappel] = i
+            if compteNoeudBTAC(solution, Xappel + 1, (JDD[0], valeurs2), noeudVisites)[0]:
+                # appel recursif de la fonction BT
+                return (True, len(noeudVisites))
+    return (False, len(noeudVisites))
