1204beb16621bb8380b626e1d30c5d351e295759
LiseIL <43322583+LiseIL@users.noreply.github.com>
2018-12-01 15:57:17 +0100

    Update calculeNbNoeudsVisites.py

--- calculeNbNoeudsVisites.py
+++ calculeNbNoeudsVisites.py
@@ -1,6 +1,5 @@
 ###################Backtracking ~ version renvoyant le nombre de noeuds parcourus##########################
-
-def nbNoeudVisitesBT(solution,Xappel,JeuDeDonnee, noeudVisites):
+def compteNoeudBT(solution,Xappel,JeuDeDonnee, nbNoeudVisites):
 #input:
 #------
 #solution: une liste de n zéros, où n est le nombre de variables dans le JeuDeDonnee
@@ -9,21 +8,22 @@ def nbNoeudVisitesBT(solution,Xappel,JeuDeDonnee, noeudVisites):
 #noeudVisites : une liste vide
 
 #renvoie un tuple : (bool, int), le booleen indique True s'il existe une solution, et False sinon
-#                                l'entier indique le nombre de noeux visités par l'algo de recherche
+#  
 
     if Xappel>=JeuDeDonnee.shape[0]:
         print(solution)
-        return (True, len(noeudVisites))
+        return (True, len(nbNoeudVisites))
         #cas terminal
     for i in range (JeuDeDonnee[0,0].shape[0]):
       #dans le cas où le domaine de chaque variable est identique########################################################
-        noeudVisites += [(Xappel, i)]
+        nbNoeudVisites += [(Xappel, i)]
         if compatibleAllBefore(Xappel,i,JeuDeDonnee,solution):
             solution[Xappel]=i
-            if noeudVisitesBT(solution,Xappel+1,JeuDeDonnee, noeudVisites)[0]:
+            if compteNoeudBT(solution,Xappel+1,JeuDeDonnee, nbNoeudVisites)[0]:
                 #appel recursif de la fonction BT
-                return (True, len(noeudVisites))
-    return (False, len(noeudVisites))
+                return (True, len(nbNoeudVisites))
+    return (False, len(nbNoeudVisites))
+
 
 def compatibleAllBefore(Xi,i,JeuDeDonnee,solution):
 #renvoie True si la valeur de Xi est compatible avec toutes les variables précédentes : {X0, X1,...,X(i-1)}
