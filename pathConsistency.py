a8ce2de1d7b49799a3048623ed3f3a421262c4b2
Auhalf <44766869+Auhalf@users.noreply.github.com>
2018-12-02 10:18:11 +0100

    Update pathConsistency.py

--- pathConsistency.py
+++ pathConsistency.py
@@ -23,7 +23,7 @@ def withoutSupportPC(i,j,k,a,b, jeuDD):
     assert(cardinald_k <= 2)
     c = 0  #!!!Travailler avec domaine de Xk
 
-    if (not (jeuDD[i,k,a,c]==1 and jeuDD[j,k,b,c]==1)) and cardinald_k==2:
+    if (not(jeuDD[i,k,a,c]==1 and jeuDD[j,k,b,c]==1)) and cardinald_k==2:
         c = 1
 
     return not (jeuDD[i, k, a, c]==1 and jeuDD[j, k, b, c]==1)
