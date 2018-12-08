17f9f7f79eda90fce7c220c5359dff99169be740
Nicolas669 <43379455+Nicolas669@users.noreply.github.com>
2018-12-08 10:55:19 +0100

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -3,7 +3,7 @@ __Filename__ = 'Interface.py'
 from time import*
 import matplotlib.pyplot as plt
 from calculeNbNoeudsVisitesBT import*
-from calculeNbNoeudVisitesBTAC import *
+from calculeNbNoeudsVisitesBTAC import *
 from calculeNbNoeudsVisitesBigMAC import*
 from Backtracking import*
 from BTAC import*
@@ -19,7 +19,6 @@ from genererJeuDeDonnees import*
 def takeTimeNodes(jeuDD, algorithme, Xappel=0):
     """renvoie un tuple donnant le temps de calcul et le nombre de noeuds développés
     lors de l'aplication de l'algorithme 'algorithme' au jeu de données 'jeuDD'
-
     'algorithme' peut être : compteNoeudBT, compteNouedBicmac, compteNoeudBTAC"""
 
     solution = [0]*jeuDD.shape[0]
@@ -67,7 +66,8 @@ minCardD=2
 maxCardD=44
 stepCardD=4
 nbVar=15
-nbContraintes = 100
+nbContraintesmax = 100
+nbContraintesmin = 50
 tauxSatisf = 0.5
 
 l=[]
@@ -75,92 +75,156 @@ for i in range(minCardD,maxCardD,stepCardD):
     l=l+[i]
 print(l)
 
-ens = ensembleJeuDomaineVariable(minCardD, maxCardD, stepCardD, nbVar, nbContraintes, tauxSatisf)
+ensmax = ensembleJeuDomaineVariable(minCardD, maxCardD, stepCardD, nbVar, nbContraintesmax, tauxSatisf)
+ensmin = ensembleJeuDomaineVariable(minCardD, maxCardD, stepCardD, nbVar, nbContraintesmin, tauxSatisf)
 
 #Pour le Backtracking
 print("1")
-testAllTimeNodesBT = takeAllTimeNodes(ens, compteNoeudBT)
-
+testAllTimeNodesBTmax = takeAllTimeNodes(ensmax, compteNoeudBT)
+testAllTimeNodesBTmin = takeAllTimeNodes(ensmin, compteNoeudBT)
 #Pour BTAC
 print("2")
-testAllTimeNodesBTAC = takeAllTimeNodes(ens, compteNoeudBTAC)
-
+testAllTimeNodesBTACmax = takeAllTimeNodes(ensmax, compteNoeudBTAC)
+testAllTimeNodesBTACmin =  takeAllTimeNodes(ensmin, compteNoeudBTAC)
 #Pour Bigmac
 print("3")
-testAllTimeNodesBigMAC = takeAllTimeNodes(ens, compteNoeudBigmac)
+testAllTimeNodesBigMACmax = takeAllTimeNodes(ensmax, compteNoeudBigmac)
+testAllTimeNodesBigMACmin = takeAllTimeNodes(ensmin, compteNoeudBigmac)
 
-#############Proprietes de la fenetre
+#############Proprietes de la fenetre1
 
 plt.figure(figsize=(24,14), dpi=80)
-plt.suptitle('Comparaison des algorithmes',fontsize=30)
+plt.suptitle('Comparaison des temps de calcul des algorithmes',fontsize=45)
 
-#############Affichage du temps de calcul des methodes
+#############Affichage du temps de calcul des methodes max
 
 axes1=plt.subplot(211)
 
 plt.annotate(' nombre de variables :'+ str(nbVar)+
-             ' \n nombre de contraintes : '+ str(nbContraintes)+
+             ' \n nombre de contraintes : '+ str(nbContraintesmax)+
              ' \n taux de satisfiabilite : '+ str(tauxSatisf),
-             fontsize=20, xy = (0.8,0.86),
-             xycoords='figure fraction', xytext = (0.8,0.82),
-             arrowprops = {'facecolor': 'white', 'edgecolor': 'white',
-                           'width': 15, 'headwidth': 30},
-             bbox=dict(boxstyle="round,pad=0.1", facecolor="white",
-                       edgecolor="grey", lw=1,))
-
-plt.annotate('Etude du temps de calcul',fontsize=20, xy = (0.1,0.82),
+             fontsize=22, xy = (0.1,0.82),
              xycoords='figure fraction', xytext = (0.1,0.82),
              arrowprops = {'facecolor': 'white', 'edgecolor': 'white',
                            'width': 15, 'headwidth': 30},
              bbox=dict(boxstyle="round,pad=0.5", facecolor="white",
                        edgecolor="forestgreen", lw=1,))
 
-plt.plot(l,testAllTimeNodesBT[0],"o--",label='Backtracking',
+plt.plot(l,testAllTimeNodesBTmax[0],"o--",label='Backtracking',
          color="firebrick")
-plt.plot(l,testAllTimeNodesBTAC[0],"o--",
+plt.plot(l,testAllTimeNodesBTACmax[0],"o--",
          label='Backtracking+AC',color="darkorange")
-plt.plot(l,testAllTimeNodesBigMAC[0],"o--",label='BigMAC',
+plt.plot(l,testAllTimeNodesBigMACmax[0],"o--",label='BigMAC',
          color="royalblue")
-plt.legend(bbox_to_anchor=(1.01, 0), loc='lower left',
-           fontsize =14,borderaxespad=0.1)
-plt.ylabel('temps de calcul (en secondes)',fontsize=16)
+plt.ylabel('temps de calcul (en secondes)',fontsize=20)
 plt.yscale('log')
-plt.xlabel('taille du domaine',fontsize=16)
-#axes1.set_xlim(left=None, right=None, emit=True,
-#               auto=False, xmin=minCardD, xmax=None)
+plt.xlabel('taille du domaine',fontsize=20)
+
 plt.xticks(np.arange(minCardD, maxCardD, step=stepCardD))
 
-#############Affichage du nombre de noeuds des methodes
+#############Affichage du temps de calcul des methodes min
 
 axes2=plt.subplot(212)
 
-plt.annotate('Etude du nombre de noeuds visites',fontsize=20,
-             xy = (0.1,0.4),
+plt.annotate(' nombre de variables :'+ str(nbVar)+
+             ' \n nombre de contraintes : '+ str(nbContraintesmin)+
+             ' \n taux de satisfiabilite : '+ str(tauxSatisf),
+             fontsize=22, xy = (0.1,0.4),
              xycoords='figure fraction', xytext = (0.1,0.4),
              arrowprops = {'facecolor': 'white', 'edgecolor': 'white',
                            'width': 15, 'headwidth': 30},
              bbox=dict(boxstyle="round,pad=0.5", facecolor="white",
                        edgecolor="forestgreen", lw=1,))
 
-plt.plot(l,testAllTimeNodesBT[1],"o--",label='Backtracking',
+plt.plot(l,testAllTimeNodesBTmin[0],"o--",label='Backtracking',
          color="firebrick")
-plt.plot(l,testAllTimeNodesBTAC[1],"o--",label='Backtracking+AC',
-         color="darkorange")
-plt.plot(l,testAllTimeNodesBigMAC[1],"o--",label='BigMAC',
+plt.plot(l,testAllTimeNodesBTACmin[0],"o--",
+         label='Backtracking+AC',color="darkorange")
+plt.plot(l,testAllTimeNodesBigMACmin[0],"o--",label='BigMAC',
          color="royalblue")
-plt.legend(bbox_to_anchor=(1.01, 0), loc='lower left',
-           fontsize =14,borderaxespad=0.1)
-plt.ylabel('nombre de noeuds',fontsize=16)
-plt.xlabel('taille du domaine',fontsize=16)
-#axes2.set_xlim(left=None, right=None, emit=True, auto=False,
-#               xmin=minCardD, xmax=None)
+plt.legend(bbox_to_anchor=(0.8, 1.07), loc='lower left',
+           fontsize =20,borderaxespad=0.1)
+plt.ylabel('temps de calcul (en secondes)',fontsize=20)
+plt.yscale('log')
+plt.xlabel('taille du domaine',fontsize=20)
+
+plt.xticks(np.arange(minCardD, maxCardD, step=stepCardD))
+
+plt.subplots_adjust(left=0.09,right=0.88, wspace=0.1,hspace=0.5,
+                    bottom=0.1, top=0.8)
+#############Proprietes de la fenetre2
+
+plt.figure(figsize=(24,14), dpi=80)
+plt.suptitle('Comparaison du nombre de noeuds visités par nos algorithmes',fontsize=45)
+
+#############Affichage du nombre de noeuds des methodes max
+
+axes3=plt.subplot(211)
+
+plt.annotate(' nombre de variables :'+ str(nbVar)+
+             ' \n nombre de contraintes : '+ str(nbContraintesmax)+
+             ' \n taux de satisfiabilite : '+ str(tauxSatisf),
+             fontsize=22, xy = (0.1,0.82),
+             xycoords='figure fraction', xytext = (0.1,0.82),
+             arrowprops = {'facecolor': 'white', 'edgecolor': 'white',
+                           'width': 15, 'headwidth': 30},
+             bbox=dict(boxstyle="round,pad=0.5", facecolor="white",
+                       edgecolor="forestgreen", lw=1,))
+
+plt.plot(l,testAllTimeNodesBTmax[1],"o--",label='Backtracking',
+         color="firebrick")
+plt.plot(l,testAllTimeNodesBTACmax[1],"o--",label='Backtracking+AC',
+         color="darkorange")
+plt.plot(l,testAllTimeNodesBigMACmax[1],"o--",label='BigMAC',
+         color="royalblue")
+plt.ylabel('nombre de noeuds',fontsize=20)
+plt.xlabel('taille du domaine',fontsize=20)
+
 plt.xticks(np.arange(minCardD, maxCardD, step=stepCardD))
 plt.subplots_adjust(left=0.09,right=0.88, wspace=0.1,hspace=0.5,
                     bottom=0.1, top=0.8)
 
 plt.savefig("Comparaison des algorithmes_X"+ str(nbVar) +
-            "-C"+ str(nbContraintes) +"-S0,1_min-"+ str(minCardD)+
+            "-C"+ str(nbContraintesmax) +"-S0,1_min-"+ str(minCardD)+
             "-max-"+ str(maxCardD)+"-step-"+str(stepCardD))
 
-plt.show()
 
+#############Affichage du nombre de noeuds des methodes min
+
+axes4=plt.subplot(212)
+
+plt.annotate(' nombre de variables :'+ str(nbVar)+
+             ' \n nombre de contraintes : '+ str(nbContraintesmin)+
+             ' \n taux de satisfiabilite : '+ str(tauxSatisf),
+             fontsize=22, xy = (0.1,0.4),
+             xycoords='figure fraction', xytext = (0.1,0.4),
+             arrowprops = {'facecolor': 'white', 'edgecolor': 'white',
+                           'width': 15, 'headwidth': 30},
+             bbox=dict(boxstyle="round,pad=0.5", facecolor="white",
+                       edgecolor="forestgreen", lw=1,))
+
+plt.plot(l,testAllTimeNodesBTmin[1],"o--",label='Backtracking',
+         color="firebrick")
+plt.plot(l,testAllTimeNodesBTACmin[1],"o--",label='Backtracking+AC',
+         color="darkorange")
+plt.plot(l,testAllTimeNodesBigMACmin[1],"o--",label='BigMAC',
+         color="royalblue")
+plt.legend(bbox_to_anchor=(0.8, 1.07), loc='lower left',
+           fontsize =20,borderaxespad=0.1)
+plt.ylabel('nombre de noeuds',fontsize=20)
+plt.xlabel('taille du domaine',fontsize=20)
+
+plt.xticks(np.arange(minCardD, maxCardD, step=stepCardD))
+
+plt.subplots_adjust(left=0.09,right=0.88, wspace=0.1,hspace=0.5,
+                    bottom=0.1, top=0.8)
+
+########################################################################
+
+plt.savefig("Comparaison des algorithmes_X"+ str(nbVar) +
+            "-Cmax"+ str(nbContraintesmax)+
+            "-Cmin"+ str(nbContraintesmin) +"-S0,1_min-"+ str(minCardD)+
+            "-max-"+ str(maxCardD)+"-step-"+str(stepCardD))
+
+
+plt.show()
